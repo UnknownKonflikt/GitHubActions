8889ca77c9c162928f71187748d25755e6e48c58
Anthony Gillespie <unknownkonflikt@gmail.com>
2025-01-14 13:35:26 -0600

    check DB connection

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,6 +3,7 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
+    console.log(models[modelName].db.db);
     let modelExists = await models[modelName].db.db?.listCollections({
       name: collectionName
     }).toArray()
